6004cb45abacff48c54683380b03b23be09ca3ca
Janosch Machowinski <janosch.machowinski@dfki.de>
2011-02-03 10:41:23 +0100

    reworked orogen_plugin to generate code for new API
    The orogen plugin generates now proper code for the new
    API. Also the user API has changes slightly. The new
    user API looks like this :
    
    transformer do
        transformation("body", "odometry")
        transformation("body", "world")
        transformation("laser", "body")
        align_port('scan_samples', 0.025)
    
        max_latency(1.0)
    end

--- orogen_plugin/transformer_plugin.rb
+++ orogen_plugin/transformer_plugin.rb
@@ -12,10 +12,11 @@ module TransformerPlugin
 	end
 
 	def generate_parse_time_code(config)
+	    task.project.using_library('transformer', :typekit => false)
 
-	    task.property("transformer_timeout",   'double', config.timeout).
+	    task.property("transformer_max_latency",   'double', config.max_latency).
 		doc "Maximum time that should be waited for a delayed data sample to arrive"
-	    puts("Adding property transformer_timeout")
+	    puts("Adding property transformer_max_latency")
 
 	    #add period property for every data stream
 	    config.streams.each do |m|
@@ -34,20 +35,24 @@ module TransformerPlugin
 	    end
 	    
 	    #create ports for transformations
-	    #TODO what type ? /std/vector</wrappers/samples/RigidBodyState>
 	    task.input_port('static_transformations', '/std/vector</base/samples/RigidBodyState>')
 	    task.input_port('dynamic_transformations', '/base/samples/RigidBodyState').
 		needs_reliable_connection
-	    
 	end
+	
 	def generate_transformer_code(config)
 	    task.add_base_header_code("#include<transformer/Transformer.h>", true)
 	    task.add_base_member("transformer", transformer_name, "transformer::Transformer")
 
 	    task.in_base_hook("configure", "
-    #{transformer_name}.setTimeout( base::Time::fromSeconds( _transformer_timeout.value()) );
+    #{transformer_name}.setTimeout( base::Time::fromSeconds( _transformer_max_latency.value()) );
 	    ")	    
-		
+	    
+	    config.transformations.each do |t|
+		task.add_base_member("transformer", t.name , "transformer::Transformation &").
+		    initializer("#{t.name}(#{transformer_name}.registerTransformation(\"#{t.from}\", \"#{t.to}\"))")
+	    end
+	    
 	    config.streams.each do |s|
 		stream_data_type = s.get_data_type(task)
 		
@@ -55,7 +60,7 @@ module TransformerPlugin
 		task.add_base_member("transformer", s.idx_name, "int")
 		
 		#add callbacks
-		task.add_user_method("void", s.callback_name, "const base::Time &ts, const #{stream_data_type} &#{s.name}_sample, const transformer::Transformation &#{s.source_frame}2#{s.target_frame}").
+		task.add_user_method("void", s.callback_name, "const base::Time &ts, const #{stream_data_type} &#{s.name}_sample").
 		body("    throw std::runtime_error(\"Transformer callback for #{s.name} not implemented\");")
 
 		#register streams at transformer
@@ -63,16 +68,14 @@ module TransformerPlugin
 		task.in_base_hook("configure", "
     {
     const double #{s.name}Period = _#{s.name}_period.value();
-    #{s.idx_name} = #{transformer_name}.registerDataStreamWithTransform< #{stream_data_type}>(
-		    base::Time::fromSeconds(#{s.name}Period), std::string(\"#{s.source_frame}\"), std::string(\"#{s.target_frame}\"),
-		    boost::bind( &TaskBase::#{s.callback_name}, this, _1, _2, _3 ),
-		    #{s.interpolate});
+    #{s.idx_name} = #{transformer_name}.registerDataStream< #{stream_data_type}>(
+		    base::Time::fromSeconds(#{s.name}Period), boost::bind( &TaskBase::#{s.callback_name}, this, _1, _2));
     }
 		")
 		
 		task.in_base_hook("update", "
     std::vector<base::samples::RigidBodyState> staticTransforms;
-    while(_static_transformations.read(staticTransforms) == RTT::NewData) {
+    while(_static_transformations.read(staticTransforms, false) == RTT::NewData) {
 	for(std::vector<base::samples::RigidBodyState>::const_iterator it = staticTransforms.begin(); it != staticTransforms.end(); it++)
 	{
 	    #{transformer_name}.pushStaticTransformation(*it);
@@ -80,7 +83,7 @@ module TransformerPlugin
     }
 		                 
     base::samples::RigidBodyState dynamicTransform;
-    while(_dynamic_transformations.read(dynamicTransform) == RTT::NewData) {
+    while(_dynamic_transformations.read(dynamicTransform, false) == RTT::NewData) {
 	#{transformer_name}.pushDynamicTransformation(dynamicTransform);
     }    
 		                  
@@ -94,22 +97,16 @@ module TransformerPlugin
     
     class TransformerConfiguration
 	class StreamDescription
-	    def initialize(name, period, source_frame, target_frame, interpolate)
+	    def initialize(name, period)
 		@name = name
 		@period = period
-		@source_frame = source_frame
-		@target_frame = target_frame
 		@idx_name = name + "_idx_tr"
 		@callback_name = name + "TransformerCallback"
-		@interpolate = interpolate;
 	    end
 	    attr_reader :name
 	    attr_reader :period
-	    attr_reader :source_frame
-	    attr_reader :target_frame
 	    attr_reader :idx_name
 	    attr_reader :callback_name
-	    attr_reader :interpolate
 	    
 	    def get_data_type(task)
 		port = task.find_port(name)
@@ -119,33 +116,48 @@ module TransformerPlugin
 	    
 		port.type.cxx_name	    
 	    end
-
 	end
 	
-	dsl_attribute :timeout
+	class TransformationDescription
+	    attr_reader :from
+	    attr_reader :to
+	    
+	    attr_reader :name
+	    
+	    def initialize(from, to)
+		@from = from
+		@to = to
+		@name = "_"+from+"2"+to 
+	    end
+	end
+	
+	dsl_attribute :max_latency
 	attr_accessor :streams
+	attr_accessor :transformations
 
-	def add_port()
-	end
-	
 	def initialize()
 	    @streams = Array.new()
+	    @transformations = Array.new()
 	end
 	
-	def align_port_with_transformation(name, period, sourceFrame, targetFrame, interpolate = false)
-	    streams << StreamDescription.new(name, period, sourceFrame, targetFrame, interpolate)
+	def transformation(from, to)
+	    transformations.push(TransformationDescription.new(from, to))
+	end
+	
+	def align_port(name, period)
+	    streams << StreamDescription.new(name, period)
 	end
 
     end
     
-    def add_transformer(&block)
+    def transformer(&block)
 	register_port_listener_generator()
 
 	config = TransformerConfiguration.new()
 	config.instance_eval(&block)
 
-	if(!config.timeout)
-	   raise "not timeout specified for transformer" 
+	if(!config.max_latency)
+	   raise "not max_latency specified for transformer" 
 	end
 	
 	gen = TransformerGenerator.new(self)
