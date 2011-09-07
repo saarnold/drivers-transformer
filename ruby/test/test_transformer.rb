6d3e33fde32325686b4b634a9970d26cb109786e
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2011-09-07 17:59:43 +0200

    ruby: remove ambiguous case in the tests

--- ruby/test/test_transformer.rb
+++ ruby/test/test_transformer.rb
@@ -47,11 +47,11 @@ class TC_Transformer < Test::Unit::TestCase
             conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
             conf.dynamic_transform("servo_high", "servo_low", "dynamixel"),
             conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("servo_low", "body", Eigen::Vector3.new(0, 0, 0))
+            conf.static_transform("servo_high", "body", Eigen::Vector3.new(0, 0, 0))
         ]
         chain = trsf.transformation_chain("body", "laser")
-        assert_equal(transforms[0, 3], chain.links)
-        assert_equal([false, true, false], chain.inversions)
+        assert_equal([transforms[3], transforms[2]], chain.links)
+        assert_equal([true, false], chain.inversions)
     end
 
     def test_transformation_chain_with_loop_shortest_path
