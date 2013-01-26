9ee377a787b828251ec8f7c3025625e8c26ddf46
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-01-26 10:00:59 +0100

    syskit: use a subclass of Configuration for profile transformers
    This allows to plug in device/definitions from the profile and
    make #load use Roby standard file-searching functions

--- ruby/lib/transformer/syskit/extensions/profile.rb
+++ ruby/lib/transformer/syskit/extensions/profile.rb
@@ -3,8 +3,34 @@ module Transformer
     #
     # It gives access to the transformer configuration
     module ProfileExtension
+        # This is a normal transformer configuration with only a few added
+        # tweaks
+        class Configuration < Transformer::Configuration
+            attr_reader :profile
+            def initialize(profile)
+                @profile = profile
+                super()
+            end
+
+            def load(*path)
+                args = path.dup
+                args << Hash[:order => :specific_first]
+                if !Roby.app.find_file(*args)
+                    raise ArgumentError, "cannot find #{File.join(*path)}"
+                end
+                super(path)
+            end
+
+            def method_missing(m, *args, &block)
+                if m.to_s =~ /_dev$/ || m.to_s =~ /_def$/
+                    profile.send(m, *args, &block)
+                else super
+                end
+            end
+        end
+
         def transformer
-            @transformer ||= Configuration.new
+            @transformer ||= Configuration.new(self)
             if block_given?
                 @transformer.instance_eval(&proc)
             end
