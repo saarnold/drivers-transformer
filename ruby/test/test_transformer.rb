d4a00aa29e4c6928b49853c7a9495a94022d7c52
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-01-02 16:16:00 +0100

    ruby,test: fix test suite

--- ruby/test/test_transformer.rb
+++ ruby/test/test_transformer.rb
@@ -5,68 +5,43 @@ require 'eigen'
 require 'pp'
 
 class TC_Transformer < Test::Unit::TestCase
-    attr_reader :trsf
+    attr_reader :trsf, :transforms
     def conf; trsf.conf end
 
     def setup
         @trsf = Transformer::TransformationManager.new
+        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
+
+        @transforms = [
+            conf.static_transform(Eigen::Vector3.new(0, 0, 0), "body" => "servo_low"),
+            conf.static_transform(Eigen::Vector3.new(0, 0, 0), "servo_high" => "laser"),
+            conf.static_transform(Eigen::Vector3.new(0, 0, 0), "laser" => "camera")
+        ]
     end
 
     def test_simple_transformation_chain
-        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
-
-        transforms = [
-            conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
-            conf.dynamic_transform("servo_low", "servo_high", "dynamixel"),
-            conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("laser", "camera", Eigen::Vector3.new(0, 0, 0))
-        ]
+        transforms.insert(1, conf.dynamic_transform("dynamixel", "servo_low" => "servo_high"))
         chain = trsf.transformation_chain("body", "laser")
         assert_equal(transforms[0, 3], chain.links)
         assert_equal([false, false, false], chain.inversions)
     end
 
     def test_transformation_chain_with_inversions
-        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
-
-        transforms = [
-            conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
-            conf.dynamic_transform("servo_high", "servo_low", "dynamixel"),
-            conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("laser", "camera", Eigen::Vector3.new(0, 0, 0))
-        ]
+        transforms.insert(1, conf.dynamic_transform("dynamixel", "servo_high" => "servo_low"))
         chain = trsf.transformation_chain("body", "laser")
         assert_equal(transforms[0, 3], chain.links)
         assert_equal([false, true, false], chain.inversions)
     end
 
-    def test_transformation_chain_with_loop_ignored
-        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
-
-        transforms = [
-            conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
-            conf.dynamic_transform("servo_high", "servo_low", "dynamixel"),
-            conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("servo_high", "body", Eigen::Vector3.new(0, 0, 0))
-        ]
+    def test_transformation_chain_with_loop_shortest_path
+        transforms.insert(1, conf.dynamic_transform("dynamixel", "servo_high" => "servo_low"))
+        transforms << conf.static_transform(Eigen::Vector3.new(0, 0, 0), "servo_high" => "body")
         chain = trsf.transformation_chain("body", "laser")
-        assert_equal([transforms[3], transforms[2]], chain.links)
+        assert_equal([transforms[4], transforms[2]], chain.links)
         assert_equal([true, false], chain.inversions)
     end
 
-    def test_transformation_chain_with_loop_shortest_path
-        conf.frames "body", "servo_low", "servo_high", "laser", "camera", "camera_optical"
 
-        transforms = [
-            conf.static_transform("body", "servo_low", Eigen::Vector3.new(0, 0, 0)),
-            conf.dynamic_transform("servo_high", "servo_low", "dynamixel"),
-            conf.static_transform("servo_high", "laser", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("servo_high", "body", Eigen::Vector3.new(0, 0, 0)),
-            conf.static_transform("laser", "camera", Eigen::Vector3.new(0, 0, 0))
-        ]
-        chain = trsf.transformation_chain("body", "laser")
-        assert_equal([transforms[3], transforms[2]], chain.links)
-        assert_equal([true, false], chain.inversions)
     end
 end
 
